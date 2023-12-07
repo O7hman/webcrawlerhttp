56dd09abfcd3768b6b0e6b3260853991dc22bb0d
Othman <othman@Othmans-MacBook-Air.local>
2023-12-07 17:54:52 -0330

    getURLsFromHTML funcitons and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, BaseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const urlsFromHTML = dom.window.document.querySelectorAll('a')
+    for(let url of urlsFromHTML){
+        if(url.href.slice(0,1) === '/'){
+            //relative
+            try{
+                const urlObject = new URL(`${BaseURL}${url.href}`)
+                urls.push(urlObject.href)
+            }catch (err){
+                console.log(`There was an error with relative URL: ${err.message}`)
+            }
+        }else{
+            //absolute
+            try{
+                const urlObject = new URL(url.href)
+                urls.push(urlObject.href)
+            }catch (err){
+                console.log(`There was an erro with absolute URL: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString){
     const urlObject = new URL(urlString)
     const urlOutput = `${urlObject.hostname}${urlObject.pathname}` 
@@ -9,5 +37,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL 
+    normalizeURL,
+    getURLsFromHTML
 }
