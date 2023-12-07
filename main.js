1870754b96900f665427cb83f8aff6dd0a9a8ee1
Othman <othman@Othmans-MacBook-Air.local>
2023-12-07 20:01:43 -0330

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello')
+const { crawlPage } = require("./crawl")
+
+function main(){
+    if(process.argv.length < 3){
+        console.log('no website provided')
+        process.exit(1)
+    }
+    if (process.argv.length > 3){
+        console.log('too many command line args')
+        process.exit(1)
+    }
+    const BaseURL = process.argv[2]
+
+    console.log(`starting crawl of ${BaseURL}`)
+    crawlPage(BaseURL)
+}
+
+main()
