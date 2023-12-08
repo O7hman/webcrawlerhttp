367273d8956f993ebe038b26225c59106e554097
Othman <othman@Othmans-MacBook-Air.local>
2023-12-08 08:47:35 -0330

    crawl page recursive

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require("./crawl")
 
-function main(){
+async function main(){
     if(process.argv.length < 3){
         console.log('no website provided')
         process.exit(1)
@@ -9,10 +9,14 @@ function main(){
         console.log('too many command line args')
         process.exit(1)
     }
-    const BaseURL = process.argv[2]
+    const baseURL = process.argv[2]
 
-    console.log(`starting crawl of ${BaseURL}`)
-    crawlPage(BaseURL)
+    console.log(`starting crawl of ${baseURL}`)
+    const pages = await crawlPage(baseURL, baseURL, {})
+    
+    for (const page of Object.entries(pages)){
+        console.log(page)
+    }
 }
 
 main()
